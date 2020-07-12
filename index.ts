f753c2231eb8f7c5d1bb57e162571bf1aa52f1a9
Tromino Technologies <61924641+trominos@users.noreply.github.com>
2020-07-12 15:55:21 -0400

    Update index.ts

--- index.ts
+++ index.ts
@@ -1,7 +1,17 @@
 import * as eks from "@pulumi/eks";
+import * as pulumi from "@pulumi/pulumi";
+import * as aws from "@pulumi/aws";
+import * as awsx from "@pulumi/awsx";
 
-// Create an EKS cluster with the default configuration.
-const cluster = new eks.Cluster("my-cluster1");
 
-// Export the cluster's kubeconfig.
-export const kubeconfig = cluster.kubeconfig;
+const vpc = new awsx.ec2.Vpc("custom", {
+    cidrBlock: "172.16.0.0/16",
+    subnets: [
+        { type: "public" },
+        { type: "private" },
+        { type: "isolated", name: "db" },
+        { type: "isolated", name: "redis" },
+    ],
+});
+
+
