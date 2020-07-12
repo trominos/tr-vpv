c236f936fdd1b64c8fac3cedf582a5ed9d902d86
Tromino Technologies <61924641+trominos@users.noreply.github.com>
2020-07-12 15:29:55 -0400

    Update index.ts

--- index.ts
+++ index.ts
@@ -14,27 +14,14 @@ const vpc = new awsx.ec2.Vpc("custom-1", {
 });
 
 
+const allVpcSubnets = vpc.privateSubnetIds.concat(vpc.publicSubnetIds);
 
-// Allocate a security group and then a series of rules:
-const sg = new awsx.ec2.SecurityGroup("thisistestsg", { vpc });
-
-// 1) inbound SSH traffic on port 22 from a specific IP address
-sg.createIngressRule("ssh-access", {
-    location: { cidrBlocks: [ "203.0.113.25/32" ] },
-    ports: new awsx.ec2.TcpPorts(22),
-    description: "allow SSH access to 203.0.113.25",
+// Create an EKS cluster inside of the VPC.
+const cluster2 = new eks.Cluster("my-cluster", {
+    vpcId: vpc.id,
+    subnetIds: allVpcSubnets,
+    nodeAssociatePublicIpAddress: false,
 });
 
-// 2) inbound HTTPS traffic on port 443 from anywhere
-sg.createIngressRule("https-access", {
-    location: new awsx.ec2.AnyIPv4Location(),
-    ports: new awsx.ec2.TcpPorts(443),
-    description: "allow HTTPS access from anywhere",
-});
-
-// 3) outbound TCP traffic on any port to anywhere
-sg.createEgressRule("outbound-access", {
-    location: new awsx.ec2.AnyIPv4Location(),
-    ports: new awsx.ec2.AllTcpPorts(),
-    description: "allow outbound access to anywhere",
-});
+// Export the cluster's kubeconfig.
+export const kubeconfig = cluster.kubeconfig;
