7c851535f638d6f4e15db50faed206f15a7a604d
Tromino Technologies <61924641+trominos@users.noreply.github.com>
2020-07-12 15:18:35 -0400

    Update index.ts

--- index.ts
+++ index.ts
@@ -16,23 +16,3 @@ const vpc = new awsx.ec2.Vpc("custom-1", {
 // Allocate a security group and then a series of rules:
 const sg = new awsx.ec2.SecurityGroup("sg", { vpc });
 
-// 1) inbound SSH traffic on port 22 from a specific IP address
-sg.createIngressRule("ssh-access", {
-    location: { cidrBlocks: [ "203.0.113.25/32" ] },
-    ports: new awsx.ec2.TcpPorts(22),
-    description: "allow SSH access to 203.0.113.25",
-});
-
-// 2) inbound HTTPS traffic on port 443 from anywhere
-sg.createIngressRule("https-access", {
-    location: new awsx.ec2.AnyIPv4Location(),
-    ports: new awsx.ec2.TcpPorts(443),
-    description: "allow HTTPS access from anywhere",
-});
-
-// 3) outbound TCP traffic on any port to anywhere
-sg.createEgressRule("outbound-access", {
-    location: new awsx.ec2.AnyIPv4Location(),
-    ports: new awsx.ec2.AllTcpPorts(),
-    description: "allow outbound access to anywhere",
-});
